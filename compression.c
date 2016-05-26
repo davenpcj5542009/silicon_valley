7e837fbb37f7a3d112d3468539d334d32fd2379f
Chris <davenpcj5542009@users.noreply.github.com>
2016-05-25 19:50:53 -0500

    Update compression.c
    removing easter egg code, to leave the rest of the "compression" code.

--- compression.c
+++ compression.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef unsigned long u64;
-
-/* Start here */
-typedef void enc_cfg_t;
-typedef int enc_cfg2_t;
-typedef __int128_t dcf_t;
-
-enc_cfg_t _ctx_iface(dcf_t s, enc_cfg2_t i){
-  int c = (((s & ((dcf_t)0x1FULL << i * 5)) >> i * 5) + 65);
-  printf("%c", c); }
-  enc_cfg2_t main() {
-  for (int i=0; i<17; i++){
-    _ctx_iface(0x79481E6BBCC01223 + ((dcf_t)0x1222DC << 64), i);
-  }
-}
-/* End here */
+typedef unsigned long long u64;
 
 //TOneverDO change name
 u64 HammingCtr(u64 a, u64 b) {
@@ -76,3 +61,5 @@ int main() {
     u64 *out = ConvolutedMagic(x,y,z,n_samples,64);
     
     for(int i=0; i<n_samples;i++) {
+    }
+}
